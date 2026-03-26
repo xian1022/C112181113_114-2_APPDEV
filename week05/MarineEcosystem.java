e369ad50d07c958b39a0925372ae131e8b4ad494
xian1022 <C112181113@nkust.edu.tw>
2026-03-26 11:36:22 +0800

    new

--- week05/MarineEcosystem.java
+++ week05/MarineEcosystem.java
@@ -1,1 +1,148 @@
+// 父類別 Creature
+class Creature {
+    // 屬性：名稱與棲息地
+    protected String name;
+    protected String habitat;
 
+    // 建構子
+    public Creature(String name, String habitat) {
+        this.name = name;
+        this.habitat = habitat;
+    }
+
+    // 一般移動描述
+    public String move() {
+        return name + " 在海中移動";
+    }
+
+    // 一般覓食描述
+    public String eat() {
+        return name + " 在進食";
+    }
+
+    // 回傳完整描述
+    public String describe() {
+        return name + "（" + habitat + "）";
+    }
+
+    // final 方法：子類別無法覆寫
+    public final String kingdom() {
+        return "動物界";
+    }
+
+    // ---- 方法多載 (Overloading) ----
+    
+    // 版本 1：無參數
+    public String feed() {
+        return name + " 正在覓食";
+    }
+
+    // 版本 2：指定食物
+    public String feed(String food) {
+        return name + " 正在吃 " + food;
+    }
+
+    // 版本 3：指定食物和數量
+    public String feed(String food, int amount) {
+        return name + " 吃了 " + amount + " 份 " + food;
+    }
+}
+
+// 子類別 1：Shark 鯊魚
+class Shark extends Creature {
+    public Shark(String name, String habitat) {
+        super(name, habitat); // 呼叫父類別建構子
+    }
+
+    @Override
+    public String move() {
+        return name + " 高速衝刺獵食";
+    }
+
+    @Override
+    public String eat() {
+        return name + " 撕咬獵物";
+    }
+}
+
+// 子類別 2：Turtle 海龜
+class Turtle extends Creature {
+    public Turtle(String name, String habitat) {
+        super(name, habitat);
+    }
+
+    @Override
+    public String move() {
+        return name + " 緩慢划動四肢";
+    }
+
+    @Override
+    public String eat() {
+        return name + " 啃食海草";
+    }
+}
+
+// 子類別 3：Dolphin 海豚
+class Dolphin extends Creature {
+    public Dolphin(String name, String habitat) {
+        super(name, habitat);
+    }
+
+    @Override
+    public String move() {
+        return name + " 躍出水面";
+    }
+
+    @Override
+    public String eat() {
+        return name + " 合作圍捕魚群";
+    }
+}
+
+// 子類別 4：Octopus 章魚
+class Octopus extends Creature {
+    public Octopus(String name, String habitat) {
+        super(name, habitat);
+    }
+
+    @Override
+    public String move() {
+        return name + " 噴射水流推進";
+    }
+
+    @Override
+    public String eat() {
+        return name + " 用觸手捕捉獵物";
+    }
+}
+
+// 主程式類別
+public class MarineEcosystem {
+    public static void main(String[] args) {
+        // final 變數：宣告後無法再被修改
+        final int OCEAN_DEPTH = 11034;
+        System.out.println("海洋最深處：" + OCEAN_DEPTH + " 公尺\n");
+
+        // 使用父類別陣列來裝載子類別物件（展現多型 Polymorphism）
+        Creature[] ecosystem = {
+            new Shark("大白鯊", "深海"),
+            new Turtle("綠蠵龜", "珊瑚礁"),
+            new Dolphin("瓶鼻海豚", "近海"),
+            new Octopus("藍環章魚", "海底岩礁")
+        };
+
+        // 使用 for-each 迴圈走訪陣列
+        for (Creature c : ecosystem) {
+            System.out.println(c.describe());
+            System.out.println("  分類：" + c.kingdom());
+            System.out.println("  移動：" + c.move());
+            System.out.println("  覓食：" + c.eat());
+            
+            // 展示方法多載 (Overloading)
+            System.out.println("  餵食：" + c.feed());
+            System.out.println("  餵食：" + c.feed("小魚"));
+            System.out.println("  餵食：" + c.feed("小魚", 3));
+            System.out.println();
+        }
+    }
+}
