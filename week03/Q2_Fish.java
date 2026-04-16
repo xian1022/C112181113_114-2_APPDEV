7a620c40f75ff254d28a18e026709a67920ffdb6
xian1022 <C112181113@nkust.edu.tw>
2026-04-16 09:17:49 +0800

    Add files via upload

--- week03/Q2_Fish.java
+++ week03/Q2_Fish.java
@@ -1,18 +1,18 @@
-public class Q2_fish{
-    String name;
-    double weight;
+public class Q2_Fish {
+ 
+String name;
+double weight;
 
-    public void displayinf(){
-         System.out.println("This fish's name is:" + name + "Weight: "+ wieght +"KG");
-    }
-
-    public static void main(String [] args){
-        Q2_fish myfish = new Q2_fish();
-        myfish.name = "Black fish";
-        myfish.weight =250.5;
-
-        myfish.displayinf();
-
-    }
-
+public void displayInfo() {
+System.out.println("這隻魚的名字是：" + name + "，重量是：" + weight + "公斤");
+}
+public static void main(String[] args) {
+ 
+Q2_Fish myFish = new Q2_Fish();
+ 
+myFish.name = "黑鮪魚";
+myFish.weight = 250.5;
+  
+myFish.displayInfo();
+}
 }
---
+++ week03/triangle.java
@@ -0,0 +1,16 @@
+public class Triangle {
+    public static void main(String [] args){
+        printTriangle();
+    }
+
+    private static String str="";
+    private static String void printTriangle(){
+        int i.j;
+        for ( i= 1; i<= 5; i++ ){
+            for ( j=1 ; j<= i; j++){
+                str += "*";
+                str += "\n";
+            }
+    }
+    System.out.println(str);
+}
